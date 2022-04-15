b2ec998a1f14c308135473d8dbc9a5eeee96c2f0
claudi-tm <ccaducarlos@gmail.com>
2022-04-15 12:36:05 +0200

    add Tabela

--- src/controller/ControlaPessoa.java
+++ src/controller/ControlaPessoa.java
@@ -1,8 +1,39 @@
 package controller;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Vector;
+
 import ListasLigadas.No;
+import model.Pessoa;
 
 public class ControlaPessoa {
+
+    public static Vector lerFicheiro(String filename) {
+		try {
+			ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filename));
+			Vector<Pessoa> vector2 = (Vector<Pessoa>) inputStream.readObject();
+			inputStream.close();
+			return vector2;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+	}
+    
+    public static void escreverFicherio(String fileName, Vector vector) {
+		try {
+			ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName));
+			outputStream.writeObject(vector);
+			outputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
     /*public void removeCode(String id) throws Exception {
         No no = primeiro;
